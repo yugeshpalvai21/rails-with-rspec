651a2a7b1a2a5edb67fe3970fd459d7f0d844f39
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-09-15 23:03:12 +0530

    playaroung with shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Example of shallow nesting
+  # resources :posts do
+  #   resources :comments, shallow: true
+  # end
 end
